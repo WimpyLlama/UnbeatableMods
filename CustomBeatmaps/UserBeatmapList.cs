f6eee3b4e9095b5118aa5922fc3efc2e3d505657
Jake The From StateDog <34257033+WimpyLlama@users.noreply.github.com>
2021-08-04 17:15:48 -0400

    Added support for upcoming packages

--- CustomBeatmaps/UserBeatmapList.cs
+++ CustomBeatmaps/UserBeatmapList.cs
@@ -55,54 +55,69 @@ namespace CustomBeatmaps
             return false;
         }
 
+
+        public void DoBeatmaps(string fileDirc)
+        {
+            foreach (var fpath in Directory.EnumerateFiles(fileDirc))
+            {
+                // .beatmap probably doesn't exist...
+                if (fpath.EndsWith(".bytes") || fpath.EndsWith(".osu") || fpath.EndsWith(".beatmap"))
+                {
+                    var fileName = Path.GetFileNameWithoutExtension(fpath);
+                    Debug.Log($"FOUND CUSTOM BEATMAP PATH: {fileName}");
+                    var text = File.ReadAllText(fpath);
+
+
+                    var txtLines = File.ReadAllLines(fpath);
+
+                    string songName = fileName;
+                    string difficulty = "Easy";
+
+                    foreach (var line in txtLines)
+                    {
+                        if (line.Contains("Version:"))
+                        {
+                            difficulty = line.Remove(0, 8);
+                        }
+                        else if (line.Contains("Title:"))
+                        {
+                            songName = line.Remove(0, 6);
+                        }
+                    }
+
+
+                    var info = new BeatmapInfo(new TextAsset(text), songName, difficulty);
+
+                    var diffs = BeatmapIndex.defaultIndex.difficulties.ToList();
+                    if (!diffs.Contains(difficulty)) diffs.Add(difficulty);
+                    BeatmapIndex.defaultIndex.difficulties = diffs.ToArray();
+
+                    _userBeatmaps.Add(info);
+                    if (!_userSongsToFilename.ContainsKey(songName))
+                    {
+                        // We have a new song, add it.
+                        var beatmapParserEngine = new BeatmapParserEngine();
+                        var beatmap = ScriptableObject.CreateInstance<Beatmap>();
+                        beatmapParserEngine.ReadBeatmap(info.text, ref beatmap);
+                        var songFilename = beatmap.general.audioFilename;
+                        _userSongsToFilename[songName] = songFilename;
+                    }
+                }
+            }
+        }
+
+
         public void RefreshUserBeatmaps(Predicate<string> isDifficultySelectable)
         {
             if (Directory.Exists(UserBeatmapPath))
             {
                 _userBeatmaps.Clear();
-                foreach (var fpath in Directory.EnumerateFiles(UserBeatmapPath))
-                    // .beatmap probably doesn't exist...
-                    if (fpath.EndsWith(".bytes") || fpath.EndsWith(".osu") || fpath.EndsWith(".beatmap"))
-                    {
-                        var fileName = Path.GetFileNameWithoutExtension(fpath);
-                        Debug.Log($"FOUND CUSTOM BEATMAP PATH: {fileName}");
-                        var text = File.ReadAllText(fpath);
+                foreach (var fileDirc in Directory.EnumerateDirectories(UserBeatmapPath))
+                {
+                    DoBeatmaps(fileDirc);
+                }
 
-
-                        var txtLines = File.ReadAllLines(fpath);
-
-                        string songName = fileName;
-                        string difficulty = "Easy";
-
-                        foreach (var line in txtLines)
-                        {
-                            if (line.Contains("Version:"))
-                            {
-                                difficulty = line;
-                            } else if (line.Contains("Title:"))
-                            {
-                                songName = line;
-                            }
-                        }
-
-
-                        var info = new BeatmapInfo(new TextAsset(text), songName, difficulty);
-
-                        var diffs = BeatmapIndex.defaultIndex.difficulties.ToList();
-                        if (!diffs.Contains(difficulty)) diffs.Add(difficulty);
-                        BeatmapIndex.defaultIndex.difficulties = diffs.ToArray();
-
-                        _userBeatmaps.Add(info);
-                        if (!_userSongsToFilename.ContainsKey(songName))
-                        {
-                            // We have a new song, add it.
-                            var beatmapParserEngine = new BeatmapParserEngine();
-                            var beatmap = ScriptableObject.CreateInstance<Beatmap>();
-                            beatmapParserEngine.ReadBeatmap(info.text, ref beatmap);
-                            var songFilename = beatmap.general.audioFilename;
-                            _userSongsToFilename[songName] = songFilename;
-                        }
-                    }
+                DoBeatmaps(UserBeatmapPath);
             }
         }
     }
